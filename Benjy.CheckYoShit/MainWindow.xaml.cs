b9b8bc55a72d249cc30d7112edbf4ab64022433c
Carl <Carl@CARL-DESKTOP>
2020-11-13 02:30:24 +0100

    Added file reading/writing (Carl had to help with eventhandlers for the checkboxes)

--- Benjy.CheckYoShit/MainWindow.xaml.cs
+++ Benjy.CheckYoShit/MainWindow.xaml.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -26,9 +28,72 @@ namespace Benjy.CheckYoShit
         public MainWindow()
         {
             InitializeComponent();
+            ToDoItems.CollectionChanged += ToDoItems_CollectionChanged;
             lvToDoList.ItemsSource = ToDoItems;
+            foreach (var item in ReadListFromDisk())
+                ToDoItems.Add(item);
         }
+
+        private void ToDoItems_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            if(e.NewItems != null)
+            {
+                foreach(var item in e.NewItems)
+                {
+                    if(item is ToDoItem toDoItem)
+                    {
+                        toDoItem.PropertyChanged += ToDoItem_PropertyChanged;
+                    }
+                }
+            }
+
+            if (e.OldItems != null)
+            {
+                foreach (var item in e.OldItems)
+                {
+                    if (item is ToDoItem toDoItem)
+                    {
+                        toDoItem.PropertyChanged -= ToDoItem_PropertyChanged;
+                    }
+                }
+            }
+
+            WriteListToDisk(ToDoItems.ToList());
+        }
+
+        private void ToDoItem_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            WriteListToDisk(ToDoItems.ToList());
+        }
+
         public ObservableCollection<ToDoItem> ToDoItems { get; } = new();
+
+
+        private static readonly string diskFileDirectory = $@"{Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)}\Benjy.CheckYoShit";
+        private static readonly string diskFilePath = $@"{diskFileDirectory}\data.json";
+
+        public static List<ToDoItem> ReadListFromDisk()
+        {
+            if (File.Exists(diskFilePath)) //Checks if the file exists
+            {
+                string jsonText = File.ReadAllText(diskFilePath);
+                List<ToDoItem> toDoItems = JsonSerializer.Deserialize<List<ToDoItem>>(jsonText);
+                return toDoItems;
+            }
+            
+            return new();
+        }
+
+        public static void WriteListToDisk(List<ToDoItem> list)
+        {
+            if (!Directory.Exists(diskFileDirectory))
+            {
+                Directory.CreateDirectory(diskFileDirectory);
+            }
+            string jsonText = JsonSerializer.Serialize(list);
+
+            File.WriteAllText(diskFilePath, jsonText);
+        }
 
         private void bAddItem_Click(object sender, RoutedEventArgs e)
         {
--- Benjy.CheckYoShit/ToDoItem.cs
+++ Benjy.CheckYoShit/ToDoItem.cs
@@ -1,12 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace Benjy.CheckYoShit
 {
-    public class ToDoItem
+    public class ToDoItem : INotifyPropertyChanged
     {
-        public string Description { get; set; }
-        public bool IsDone { get; set; } = false;
+        private string _description;
+
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; OnPropertyChanged(); }
+        }
+
+        private bool _isDone;
+
+        public bool IsDone
+        {
+            get { return _isDone; }
+            set
+            {
+                _isDone = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        // Create the OnPropertyChanged method to raise the event
+        // The calling member's name will be used as the parameter.
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
     }
 }
