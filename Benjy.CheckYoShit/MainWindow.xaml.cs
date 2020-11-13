2e517ea389da8fea4fa7cd3f80984b89ad1fae50
Carl <Carl@CARL-DESKTOP>
2020-11-13 01:29:00 +0100

    Epic

---
+++ Benjy.CheckYoShit/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Benjy.CheckYoShit
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            lvToDoList.ItemsSource = ToDoItems;
+        }
+        public ObservableCollection<ToDoItem> ToDoItems { get; } = new();
+
+        private void bAddItem_Click(object sender, RoutedEventArgs e)
+        {
+            AddToList(); 
+        }
+
+        private void AddToList()
+        {
+            if (!string.IsNullOrEmpty(tbDescription.Text))
+            {
+                var item = new ToDoItem()
+                {
+                    Description = tbDescription.Text
+                };
+
+                ToDoItems.Add(item);
+                tbDescription.Text = string.Empty;
+            }
+        }
+
+        private void lvToDoList_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (sender is ListView listView && listView.SelectedItem is ToDoItem item)
+            {
+                if(e.Key == Key.Delete)
+                {
+                    ToDoItems.Remove(item);
+                }
+            }
+        }
+
+        private void tbDescription_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if(sender is TextBox tbDescription)
+            {
+                if(e.Key == Key.Enter)
+                {
+                    AddToList();
+                    e.Handled = true;
+                }
+            }
+        }
+    }
+}
---
+++ Benjy.CheckYoShit/ToDoItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Benjy.CheckYoShit
+{
+    public class ToDoItem
+    {
+        public string Description { get; set; }
+        public bool IsDone { get; set; } = false;
+    }
+}
